35bb1fc528cbc1f30f87e69822e2a67a36141631
Bilwa Gutthi <bilwa.gutthi@gmail.com>
2021-02-09 18:01:43 +0530

    Updates

--- urlShortner/Shortner/models.py
+++ urlShortner/Shortner/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Url(models.Model):
-    link = models.CharField(max_length=10000, primary_key=True)
+    link = models.CharField(max_length=10000, unique=True)
     uuid = models.CharField(max_length=10)
